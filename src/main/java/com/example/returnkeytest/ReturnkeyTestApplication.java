6857566a8c1a7e1263e23f2fc2c7c094170e3a8e
Rendy Eza Putra <rendy.putra@binus.ac.id>
2022-08-20 17:38:12 +0700

    Initial Commit

---
+++ src/main/java/com/example/returnkeytest/ReturnkeyTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.returnkeytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReturnkeyTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReturnkeyTestApplication.class, args);
+    }
+
+}
